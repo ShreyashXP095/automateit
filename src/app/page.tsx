87d2032adc4bf6bf8226251fda84e5433c171e81
Rachit Sharma <sharmarachit941@gmail.com>
2026-03-21 16:15:02 +0530

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 // landing page 
 import {requireAuth} from "@/lib/auth-utils"
 import {caller} from "@/trpc/server"
+import { LogoutButton } from "./logout";
 const Page = async ()=> {
 
   await requireAuth();
   const data = await caller.getUsers();
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center flex-col gap-y-6">
        protected server components
-       {JSON.stringify(data)}
+       <div>
+          {JSON.stringify(data)}
+       </div>
+       <LogoutButton />
     </div>
   );
 };
--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -91,6 +91,7 @@ export function RegisterForm(){
                                     type="button"
                                     disabled = {isPending}
                                 >
+                                <Image alt="GitHub" src="/logos/github.svg" width={20} height={20}/>    
                                     Continue with Github
                                 </Button>
                                 <Button
@@ -99,6 +100,7 @@ export function RegisterForm(){
                                     type="button"
                                     disabled = {isPending}
                                 >
+                                <Image alt="Google" src="/logos/google.svg" width={20} height={20}/>    
                                     Continue with Google
                                 </Button>
                             </div>
