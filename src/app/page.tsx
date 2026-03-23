699662736137b51b818349ab61883b0e0c0942cb
Rachit Sharma <sharmarachit941@gmail.com>
2026-03-24 03:31:26 +0530

    background Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
+"use client";
 // landing page 
-import {requireAuth} from "@/lib/auth-utils"
-import {caller} from "@/trpc/server"
-import { LogoutButton } from "./logout";
-const Page = async ()=> {
-
-  await requireAuth();
-  const data = await caller.getUsers();
+import {useMutation , useQuery} from "@tanstack/react-query"
+import {useTRPC} from "@/trpc/client"
+import { LogoutButton } from "./logout"
+import {toast} from "sonner";
+import {Button} from "@/components/ui/button"
+// import { useQueryClient } from "@tanstack/react-query";
+const Page =  ()=> {
+  const trpc = useTRPC();
+  const {data} = useQuery(trpc.getWorkflows.queryOptions());
+  // const queryClient = useQueryClient();
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+      onSuccess : ()=>{
+        toast.success("Job Queued");
+      }
+  }));
   return (
-    <div className="min-h-screen flex items-center justify-center flex-col gap-y-6">
+    <div className="min-h-screen flex items-center justify-center flex-col gap-y-6 bg-gray-900 text-white">
        protected server components
        <div>
           {JSON.stringify(data)}
        </div>
+       <Button disabled={create.isPending} onClick={()=>create.mutate()}>
+        Create Workflow
+        </Button>
        <LogoutButton />
     </div>
   );
---
+++ src/inngest/functions.ts
@@ -0,0 +1,22 @@
+import {inngest} from "./client";
+import prisma from "@/lib/db";
+
+export const helloWorld = inngest.createFunction(
+    {
+        id : "hello-world",
+    },
+    {
+        event: "test/hello.world"
+    },
+    async ({event , step})=>{
+        await step.sleep("wait-a-moment","1s");
+        
+        await step.run("create-workflow", ()=>{
+            return prisma.workflow.create({
+                data : {
+                    name : "working-from-inngest",
+                },
+            });
+        });
+    },
+);  
